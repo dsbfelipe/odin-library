81fc28b4716b9c98b46b1b6059ea0344ae2db946
Felipe dos Santos Bento <dsbfelipe@outlook.com>
2024-09-24 02:28:59 -0300

    Solved 'Object and Object Constructors exercise'

---
+++ script.js
@@ -0,0 +1,15 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${
+      this.read ? "read" : "not read yet"
+    }`;
+  };
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+console.log(theHobbit.info());
